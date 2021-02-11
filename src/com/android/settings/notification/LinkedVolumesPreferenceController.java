8aa27987f03c1c0de2117556bcaca69b4226718f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-11 16:21:49 +0530

    Settings: Change title of ring volume pref when unlinked
     * make it show "Ring volume" instead of "Ring & notification"
       volume when notification and ring volumes are unlinked

--- src/com/android/settings/notification/LinkedVolumesPreferenceController.java
+++ src/com/android/settings/notification/LinkedVolumesPreferenceController.java
@@ -18,16 +18,38 @@ package com.android.settings.notification;
 
 import android.content.Context;
 
+import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.notification.VolumeSeekBarPreference;
 
-public class LinkedVolumesPreferenceController extends BasePreferenceController {
+public class LinkedVolumesPreferenceController extends BasePreferenceController
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final string KEY_RING_VOLUME = "ring_volume";
+    private VolumeSeekBarPreference mRingVolumePreference;
+
     public LinkedVolumesPreferenceController(Context context, String key) {
         super(context, key);
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRingVolumePreference = (VolumeSeekBarPreference) findPreference(KEY_RING_VOLUME);
+    }
+
+    @Override
     public int getAvailabilityStatus() {
         return Utils.isVoiceCapable(mContext) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if ((Boolean) newValue) {
+            mRingVolumePreference.setTitle(R.string.ring_only_volume_option_title);
+        } else {
+            mRingVolumePreference.setTitle(R.string.ring_volume_option_title);
+        }
     }
 }
